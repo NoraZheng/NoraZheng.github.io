d630a2d79cd61505559a0e9080100052957db8c7
Nora Zheng <zhengnuo@gmail.com>
2019-09-23 19:58:48 -0400

    smoothState.js tested

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
-$(document).ready(function() {
-	// document ready
+$(function() {
+	$('#main').smoothState();
 
+	//document ready
 	$('.menu-button').on('click', () => {
+		console.log('hi');
 		$('.menu').toggleClass('show-menu');
 		$('.menu-button').toggleClass('menu-button-close');
 		$('main').toggleClass('hide-main');
 	});
-}); // end of document ready
+	// end of document ready
+});
