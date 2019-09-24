1a4e001403ce6087b14efab2dd3c397bd04ead6e
Nora Zheng <zhengnuo@gmail.com>
2019-09-24 15:49:40 -0400

    ditched font awesome CDN

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,6 @@ $(function() {
 
 	//document ready
 	$('.menu-button').on('click', () => {
-		console.log('hi');
 		$('.menu').toggleClass('show-menu');
 		$('.menu-button').toggleClass('menu-button-close');
 		$('main').toggleClass('hide-main');
